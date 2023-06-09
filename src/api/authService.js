eea0ba79158076655e438ea9240a28c36f221df9
Yana <jana.ru.sidorova@yandex.ru>
2023-06-09 13:19:53 +0300

    HOS-127: Update packages for service's communication

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,40 +1,35 @@
-const got = require('got');
+const axios = require('axios');
+const { ERRORS } = require('../translates');
 
 const { DOCKER_AUTH_SERVICE_URL } = process.env;
 const baseUrl = `http://${DOCKER_AUTH_SERVICE_URL}/internal`;
 
-const getOptions = (token, body = {}) => ({
+const getOptions = (token) => ({
   headers: { 'x-access-token': token },
-  json: true,
-  body,
 });
 
-exports.checkAuth = async (token) => {
-  const response = await got.get(`${baseUrl}/auth`, getOptions(token));
-  return response.body;
+const get = async (url, options) => {
+  try {
+    const response = await axios.get(url, options);
+    return response.data.data;
+  } catch (err) {
+    throw new Error(ERRORS.EXTERNAL_SERVICE_ERROR);
+  }
 };
 
-exports.getUser = async (token) => {
-  const response = await got.get(`${baseUrl}/users/me`, getOptions(token));
-  return response.body.data;
+const post = async (url, body, options) => {
+  try {
+    const response = await axios.post(url, body, options);
+    return response.data.data;
+  } catch (err) {
+    throw new Error(ERRORS.EXTERNAL_SERVICE_ERROR);
+  }
 };
 
-exports.getPermissions = async (token) => {
-  const response = await got.get(`${baseUrl}/permissions/me`, getOptions(token));
-  return response.body.data;
-};
+exports.checkAuth = async (token) => get(`${baseUrl}/auth`, getOptions(token));
+exports.getUser = async (token) => get(`${baseUrl}/users/me`, getOptions(token));
+exports.getPermissions = async (token) => get(`${baseUrl}/permissions/me`, getOptions(token));
 
-exports.getUsersByIds = async (token, usersIds) => {
-  const response = await got.post(`${baseUrl}/users`, getOptions(token, { ids: usersIds }));
-  return response.body.data;
-};
-
-exports.getGuestsByIds = async (token, guestIds) => {
-  const response = await got.post(`${baseUrl}/guests`, getOptions(token, { ids: guestIds }));
-  return response.body.data;
-};
-
-exports.getOrCreateGuest = async (token, body) => {
-  const response = await got.post(`${baseUrl}/guest`, getOptions(token, body));
-  return response.body.data;
-};
+exports.getUsersByIds = async (token, usersIds) => post(`${baseUrl}/users`, { ids: usersIds }, getOptions(token));
+exports.getGuestsByIds = async (token, guestIds) => post(`${baseUrl}/guests`, { ids: guestIds }, getOptions(token));
+exports.getOrCreateGuest = async (token, body) => post(`${baseUrl}/guest`, body, getOptions(token));
