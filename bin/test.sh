2f016136abe4f2f96b629ae420987a35189f8b9f
yana617 <31696608+yana617@users.noreply.github.com>
2023-09-07 00:56:42 +0300

    HOS-fix: add mongo auth (#18)
    * HOS-fix: add mongo auth
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * investigation
    
    * fix tests
    
    * update github-actions versions
    
    * update github-actions versions
    
    * fix tests

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,11 @@
 docker-compose up -d mongo-db
 
-export MONGO_DB=test-db
 export MONGO_PORT=27018
 export MONGO_HOSTNAME=localhost
+export MONGODB_DATABASE=test-db
 
 echo "start running tests"
+npx browserslist@latest --update-db
 jest --coverage --runInBand
 echo "tearing down all containers"
 
