81d8801be128d20cb4f18e60944bea352fe75e4c
Yana <jana.ru.sidorova@yandex.ru>
2023-09-06 22:50:54 +0300

    fix tests

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,14 @@
 docker-compose up -d mongo-db
 
-export MONGO_DB=test-db
 export MONGO_PORT=27018
 export MONGO_HOSTNAME=localhost
+
+export MONGO_INITDB_ROOT_USERNAME=root
+export MONGO_INITDB_ROOT_PASSWORD=rootPassword
+export MONGO_INITDB_DATABASE=test-db
+export MONGODB_USERNAME=root
+export MONGODB_PASSWORD=rootPassword
+export MONGODB_DATABASE=test-db
 
 echo "start running tests"
 jest --coverage --runInBand
