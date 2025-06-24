6dde8c4d4e904cfac0dea458038afabe637eda01
Yana <ysidarava@gmail.com>
2025-06-24 11:48:24 +0300

    fix: deploy

--- src/database/connect.js
+++ src/database/connect.js
@@ -9,7 +9,7 @@ const {
   NODE_ENV,
 } = process.env;
 
-let url = `mongodb://${MONGODB_USERNAME}:${encodeURIComponent(MONGODB_PASSWORD)}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGODB_DATABASE}?authSource=admin`;
+let url = `mongodb://${MONGODB_USERNAME}:${encodeURIComponent(MONGODB_PASSWORD)}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGODB_DATABASE}?authSource=${MONGODB_DATABASE}`;
 
 if (NODE_ENV === 'test') {
   url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGODB_DATABASE}`;
